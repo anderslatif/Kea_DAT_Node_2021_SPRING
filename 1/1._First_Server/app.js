32f9734efaef9a72ee4159efbe571e456affc2ba
Anders Latif <andl@kea.dk>
2021-02-18 11:42:45 +0100

    Recreated the first server

--- 1/1._First_Server/app.js
+++ 1/1._First_Server/app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
+// const app = require("express")();
 
-// to solve the assignment create a datatype to store the data in
-// implement the two GET endpoints .. you can hardcode the id for now
 
 app.get("/", (req, res) => {
-    res.send({ message: "This is my response" });
+    res.send({});
 });
 
-app.get("/newOne/id", (req, res) => {
-    const id = 1;
-    res.send({ "something": "That you can do" });
+// create a new route called welcome that shows an HTML header tag that says Welcome
+// the endpoint is /welcome
+// <h1>Welcome</h1>
+app.get("/welcome", (req, res) => {
+    res.send("<h1>Welcome</h1>");
+});
+
+// create a new route on the endpoint me that returns a json object that represents you
+app.get("/me", (req, res) => {
+    const me = {
+        hungryLevel: 9001 - 2
+    };
+    res.send(me);
 });
 
 app.listen(8080);
