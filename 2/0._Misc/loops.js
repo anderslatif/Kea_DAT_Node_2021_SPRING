d4185fe1e7a1b6e7f47dab86b35b4729bb985294
Anders Latif <andl@kea.dk>
2021-02-25 14:09:04 +0100

    Showed loop methods and implemented GET /day and how to serve an HTML string

---
+++ 2/0._Misc/loops.js
@@ -0,0 +1,21 @@
+// We aim to use functional approaches when looping
+// like map, filter, reduce, find etc.
+
+const failures = ["mislick", "falling down", "skateboard accident", "tiktok fail"];
+
+// loop through this array using map print out each element
+// failures.map((failure, index) => console.log(failure, index));
+
+/* const newFailures = failures.map((failure) => {
+    return { failure, hilarityLevel: 5 };
+}); */
+
+// failures[1] = "Charlie Chaplin fall";
+
+const newFailures = failures.map((failure) => failure);
+
+console.log(newFailures);
+
+// Create a new express server from scratch.. this will be a pure Data API
+// so I will call it 2._Data_API
+
--- 2/1._First_Server/app.js
+++ 2/1._First_Server/app.js
@@ -16,5 +16,23 @@ app.get("/me", (req, res) => {
     res.send(me);
 });
 
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.get("/day", (req, res) => {
+    // can also be solved like this:
+    // new Date().toLocaleDateString("da-dk", { weekday: "long" })
+    const currentWeekday = new Date().getDay();
+    res.send({ day: weekdays[currentWeekday] });
+});
+
+app.get("/about", (req, res) => {
+    res.send({
+        version: "1.0.0"
+    });
+});
+
+app.get("/page", (req, res) => {
+    res.send("<h1>Welcome</h1>");
+});
 
 app.listen(8080);
