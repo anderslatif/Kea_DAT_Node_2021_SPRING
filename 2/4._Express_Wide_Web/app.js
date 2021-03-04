795ad46e26f08be714253572a62b7d82d7e4f399
Anders Latif <andl@kea.dk>
2021-03-04 15:47:47 +0100

    Created the express wide web project for class B

---
+++ 2/4._Express_Wide_Web/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/welcome.html");
+});
+
+app.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080);
+});
