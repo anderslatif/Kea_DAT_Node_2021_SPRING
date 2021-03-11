76dd1e1c049d9595d84e5f7bbb6395028ef12f82
Anders Latif <andl@kea.dk>
2021-03-11 15:47:22 +0100

    Added client-side and server-side redirection to Express Wide Web

--- 2/4._Express_Wide_Web/app.js
+++ 2/4._Express_Wide_Web/app.js
@@ -5,6 +5,22 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/welcome.html");
 });
 
+app.get("/pizza", (req, res) => {
+    res.sendFile(__dirname + "/public/pizzaparlor.html");
+});
+
+app.get("/whiskey", (req, res) => {
+    res.send({ barkeep: "Here ya go, pal. A whiskey for you." });
+});
+
+app.get("/pub", (req, res) => {
+    if (Number(req.query.money) > 500) {
+        res.redirect("/whiskey");
+    } else {
+        res.send({ doorman: "Sorry, you don't have money for whiskey." });
+    }
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, (error) => {
