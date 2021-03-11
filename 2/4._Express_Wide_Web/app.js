9c387c845b75d7aacccb693cbb981d62a50a3a40
Anders Latif <andl@kea.dk>
2021-03-11 13:39:22 +0100

    Implemented cross-env in Express Wide Web

--- 2/4._Express_Wide_Web/app.js
+++ 2/4._Express_Wide_Web/app.js
@@ -5,9 +5,11 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/welcome.html");
 });
 
-app.listen(8080, (error) => {
+const port = process.env.PORT || 8080;
+
+app.listen(port, (error) => {
     if (error) {
         console.log(error);
     }
-    console.log("Server is running on port", 8080);
+    console.log("Server is running on port", Number(port));
 });
