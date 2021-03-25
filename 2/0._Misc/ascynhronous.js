41f49478b2b2fb4010cd5aeb360057e95dfeb244
Anders Latif <andl@kea.dk>
2021-03-25 16:15:09 +0100

    Finished converting the cat facts API call to fetch with Promises and async/await

--- 2/0._Misc/ascynhronous.js
+++ 2/0._Misc/ascynhronous.js
@@ -8,3 +8,42 @@
 // user input 
 // Files 
 
+// two states
+// pending
+// fulfilled
+    // resolved, rejected
+
+/* new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("I'm so happy!");
+    }, 4000);
+}).then(message => {
+    console.log(message);
+}); */
+
+function myPromise() {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                resolve("The mitochondria is the power house of the cell.");
+            }, 5000);
+        } catch {
+            reject("Every Promise don't work out this way.");
+        }
+    });
+}
+
+/* myPromise()
+.then(message => console.log(message))
+.catch(errorMessage => console.log(errorMessage)); */
+
+(async function myAsynchronousFunction() {
+    try {
+        const message = await myPromise();
+        console.log(message);
+    } catch (errorMessage) {
+        console.log(errorMessage);
+    }
+})()
+
+
