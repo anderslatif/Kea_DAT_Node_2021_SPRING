faf618b49c9c94ce47331483e9c4e323de1493c7
Anders Latif <andl@kea.dk>
2021-04-08 15:47:33 +0200

    Implemented SSR for the frontpage with the bun of header and footer

--- 2/5._Nodefolio/app.js
+++ 2/5._Nodefolio/app.js
@@ -4,12 +4,34 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
+const fs = require("fs");
+
+const header = fs.readFileSync(__dirname + "/public/header/header.html", "utf-8");
+const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
+
+const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
+
+// Server-side rendering
+// read the html file as text
+// send the text content to the client
+
+// In node there are two ways to read a file
+// async : correct 99.9% of the time
+// sync  : <- use this here cause it's not a problem that the server is blocked when it starts
+            // cause we need the html before the routes should be accessible anyway
+
+            /* Task */
+    // serve the frontpage by server-side rendering it
+
+// SEO 
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+    res.send(header + frontpage + footer);
 });
 
-
-/* Create and serve the projects page with nav and footer */
+app.get("/projects", (req, res) => {
+    res.sendFile(__dirname + "/public/projects/projects.html");
+});
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
     if (error) {
