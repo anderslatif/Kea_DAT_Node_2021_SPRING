f5870d8762bff498b7b62e6c91e13167babf9c72
Anders Latif <andl@kea.dk>
2021-04-15 11:47:01 +0200

    Implemented the contact form

--- 1/5._Nodefolio/app.js
+++ 1/5._Nodefolio/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static("public"));
 
 const projectsRouter = require("./routes/projects");
+const contactRouter = require("./routes/contact");
 
 app.use(projectsRouter.router);
+app.use(contactRouter.router);
 
 const fs = require("fs");
 
@@ -16,6 +19,7 @@ const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8"
 
 const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
 const projects = fs.readFileSync(__dirname + "/public/projects/projects.html", "utf-8");
+const contact = fs.readFileSync(__dirname + "/public/contact/contact.html", "utf-8");
 
 app.get("/", (req, res) => {
     res.send(nav + frontpage + footer);
@@ -23,6 +27,10 @@ app.get("/", (req, res) => {
 
 app.get("/projects", (req, res) => {
     res.send(nav + projects + footer);
+});
+
+app.get("/contact", (req, res) => {
+    res.send(nav + contact + footer);
 });
 
 const server = app.listen(process.env.PORT || 8080, (error) => {
