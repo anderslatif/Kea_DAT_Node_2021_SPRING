12c93a7c05f2b0d72f4c2b6899204c79462525de
Anders Latif <andl@kea.dk>
2021-04-15 12:46:03 +0200

    Created a simple example for a templating language

--- 2/5._Nodefolio/app.js
+++ 2/5._Nodefolio/app.js
@@ -11,19 +11,13 @@ const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8"
 
 const frontpage = fs.readFileSync(__dirname + "/public/frontpage/frontpage.html", "utf-8");
 
-// Server-side rendering
-// read the html file as text
-// send the text content to the client
+const basetemplate = fs.readFileSync(__dirname + "/public/basetemplate.html", "utf-8");
+const koalapage = basetemplate.replace("{{BODY}}", frontpage).replace("{{TITLE}}", "Best Koala title ever").replace("{{KOALA_FACT}}", "<h1>Did you know that Koalas are always and thus black, white and asian.</h1>");
 
-// In node there are two ways to read a file
-// async : correct 99.9% of the time
-// sync  : <- use this here cause it's not a problem that the server is blocked when it starts
-            // cause we need the html before the routes should be accessible anyway
+app.get("/koala", (req, res) => {
+    res.send(koalapage);
+});
 
-            /* Task */
-    // serve the frontpage by server-side rendering it
-
-// SEO 
 
 app.get("/", (req, res) => {
     res.send(header + frontpage + footer);
