56a1561a535e50dccf74eef17e464e326318aff1
Anders Latif <andl@kea.dk>
2021-04-29 11:21:29 +0200

    Created the scaffold for a socket implementation

---
+++ 1/7._Sockets/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+    console.log("A socket connected", socket.id);
+});
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/colors.html");
+});
+
+server.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server running on port", 8080);
+});
