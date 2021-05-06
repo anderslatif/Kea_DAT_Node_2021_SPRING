b67edd14fc7ee5e1ff7baa30361c8d466119c40d
Anders Latif <andl@kea.dk>
2021-05-06 13:01:04 +0200

    Implemented escaping html characters

--- 2/7._Sockets/app.js
+++ 2/7._Sockets/app.js
@@ -5,12 +5,15 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
+const escapeHtml = require("html-escaper").escape;
+
+
 io.on("connection", (socket) => {
     // console.log("A socket connected with id", socket.id);
 
     socket.on("colorChanged", (data) => {
         // changes the color for ALL the sockets in the io namespace
-        io.emit("changeBackgroundToThisColor", data);
+        io.emit("changeBackgroundToThisColor", { color: escapeHtml(data.color)});
 
         // changes the color ONLY for the socket that made the change
         // socket.emit("changeBackgroundToThisColor", data);
@@ -18,6 +21,11 @@ io.on("connection", (socket) => {
         // changes the color for ALL the sockets EXCEPT itself
         // socket.broadcast.emit("changeBackgroundToThisColor", data);
     });
+
+    socket.on("disconnect", () => {
+        console.log("A socket disconnect");
+    });
+
 });
 
 app.get("/", (req, res) => {
