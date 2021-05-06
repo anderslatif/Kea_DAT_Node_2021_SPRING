c5e8661ddff268d747f465e9a5110a2796db5fef
Anders Latif <andl@kea.dk>
2021-05-06 15:03:22 +0200

    Implemented the rate-limiter and session

--- 2/8._Express_Plus/app.js
+++ 2/8._Express_Plus/app.js
@@ -1,7 +1,32 @@
 import express from "express";
 const app = express();
 
+/* 
+How to get an external library in our HTML files
+- Get from the CDN
+- Put in our client in a script file and include it (serve statically)
+- Get from NPM - Copy files over from NPM manually
+                - Point to the file from the html script tag (src) .. remember to serve that single file statically / folder 
+*/
 
+import helmet from "helmet";
+app.use(helmet());
+
+import rateLimit from "express-rate-limit";
+const baseLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10 // limit each IP to 10 requests per windowMs
+});
+app.use(baseLimiter);
+app.use("/auth/*", authLimiter);
+
+app.get("/auth/login", (req, res) => {
+    res.send({ message: "Logging in..." });
+});
 
 const ipLogger = (req, res, next) => {
     console.log("The client IP is:", req.ip);
@@ -10,6 +35,18 @@ const ipLogger = (req, res, next) => {
 };
 
 app.use(ipLogger);
+
+import session from "express-session";
+app.use(session({
+    secret: 'keyboard cat is my favorite cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+
+import sessionRouter from './router/session.mjs';
+app.use(sessionRouter);
 
 
 app.get("/", (req, res, next) => {
@@ -24,6 +61,10 @@ app.get("/", (req, res) => {
 
 app.get("/modifiedByMyCustomMiddleware", (req, res) => {
     res.send(req.myData);
+});
+
+app.get("/*", (req, res) => {
+    res.status(404).send(`<h1>Sorry but this page was not found.</h1>`);
 });
 
 app.listen(8080, (error) => {
---
+++ 2/8._Express_Plus/router/session.mjs
@@ -0,0 +1,15 @@
+// const router = require("express").Router();
+import express from "express";
+const router = express.Router();
+
+router.get("/setsession", (req, res) => {
+    req.session.date = new Date();
+    res.send({});
+});
+
+router.get("/getsession", (req, res) => {
+    console.log(req.session.date);
+    res.send({});
+});
+
+export default router;
